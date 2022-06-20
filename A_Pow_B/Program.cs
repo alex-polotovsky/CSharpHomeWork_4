a14bacccb98e0b4a480f40b2d29a81434e1ba04c
alexandr polotovsky <apolotovsky@gmail.com>
2022-06-21 02:08:13 +0300

    add A_Pow_B, SumDigitOfNumber, ArrayGenerator

---
+++ A_Pow_B/Program.cs
@@ -0,0 +1,34 @@
+﻿/* Напишите цикл, который принимает на вход
+два числа (A и B) и возводит число A в натуральную
+степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16 */
+
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+
+Console.Write("Введите число В: ");
+int B = int.Parse(Console.ReadLine());
+
+int numPow = A;
+
+if (B < 1)
+{
+    Console.WriteLine("Out of range.");
+}
+
+else
+{
+   for (int i = 2; i <= B; i++)
+   {
+        numPow = numPow * A;
+   }
+   Console.WriteLine("А в степени В равно: "+ numPow);
+}
+
+
+
+
+
+
+
---
+++ ArrayGenerator/Program.cs
@@ -0,0 +1,26 @@
+﻿/* Задача 29: Напишите программу, которая задаёт массив
+из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33] */
+
+void PintArr(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+void FillArr(int[] array, int min, int max)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+}
+
+
+int[] list = new int[8];
+FillArr(list, 0, 100);
+PintArr(list); 
